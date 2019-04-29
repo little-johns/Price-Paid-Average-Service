1d5be8ada2ae2137f0aa6253f2e4f51265d5a78c
Kevin Yang <kkaiyang94@gmail.com>
2019-04-29 14:50:29 -0700

    Add path for get

--- server/server.js
+++ server/server.js
@@ -1,39 +1,23 @@
+require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
-
+// const sequalize = require('../database/StockPricePaid/sequalizeHelpers.js')
+const pool = require('../queries.js').pool;
 const app = express();
 const port = process.env.PORT || 8080;
 const db = require('../database/index.js');
-
+const getStockById = require('./controller/querys.js').getStockById;
 app.use(express.static(`${__dirname}/../public/`));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use('/:id', express.static(path.join(__dirname, '../public')));
 
-priceData = []
-
-app.get('/:id', (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname, '../public/index.html'));
-});
-
-app.get('/api/price', (req, res) => {
-  // set Default data equal to 001
-    db.getPaidPrice("001", (data) => {
-      res.status(200).json(data)
-    })
-});
-
-app.get('/api/price/:id', (req, res) => {
-  // set Default data equal to 001
-    db.getPaidPrice(req.params.id, (data) => {
-      res.status(200).json(data)
-    })
-});
+app.get('/api/:id', getStockById);
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
