0fab02ed862c826636f90dcc5613892995fd2c3c
Kevin Yang <kkaiyang94@gmail.com>
2019-04-29 11:30:50 -0700

    Implement Fetch

--- client/src/AveragePricePaid/averagePrice.jsx
+++ client/src/AveragePricePaid/averagePrice.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import Chart from './chart.jsx';
-import $ from 'jquery';
-
-const port = '52.53.224.110';
-const path = window.location.pathname;
 
 class AveragePrice extends React.Component {
   constructor() {
@@ -14,13 +10,23 @@ class AveragePrice extends React.Component {
   }
 
   componentDidMount() {
-    $.get(`http://${port}:3001/api/price${path}`, (stockData) => {
-      const priceData = [];
-      stockData.map(stock => priceData.push(stock.price));
-      this.setState({
-        price: priceData,
-      });
-    });
+    const weburl = window.location.pathname;
+    const id = weburl.split('/')[1];
+    const endpoint = `/api/${id}`;
+    fetch(endpoint, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then((query) => {
+        const priceData = [];
+        query.map(stock => priceData.push(parseFloat(stock.price.slice(1).replace(',', ''))));
+        this.setState({
+          price: priceData,
+        });
+      })
+      .catch(error => console.log(error));
   }
 
   render() {
@@ -46,12 +52,12 @@ class AveragePrice extends React.Component {
       let highLight = false;
       let averageLine = false;
       let occurence = sortPriceData.filter(price => price >= tempLow && price <= tempHigh).length;
-      occurence === 0 ? occurence = 1: null;
-      currentPrice > averagePrice 
-      ? averagePrice < tempLow - barRange && tempHigh - barRange <= currentPrice
-        ? highLight = true : null
-      : averagePrice > tempLow - 2 * barRange && tempHigh > currentPrice + barRange
-        ? highLight = true : null;
+      occurence === 0 ? occurence = 1 : null;
+      currentPrice > averagePrice
+        ? averagePrice < tempLow - barRange && tempHigh - barRange <= currentPrice
+          ? highLight = true : null
+        : averagePrice > tempLow - 2 * barRange && tempHigh > currentPrice + barRange
+          ? highLight = true : null;
       i === currentSpot ? averageLine = true : null;
       allData.push([occurence, highLight, averageLine, i]);
     }
@@ -60,9 +66,9 @@ class AveragePrice extends React.Component {
     const curPriceDis = currentSpot * 20.3 + 9.3;
     // find the spot for average price
     if (averagePrice > currentPrice) {
-      avgPriceDis = (averageSpot - 4.5 )* 20.3;
+      avgPriceDis = (averageSpot - 4.5) * 20.3;
     } else {
-      avgPriceDis = (averageSpot - 4.5 ) * 20.3 - 4;
+      avgPriceDis = (averageSpot - 4.5) * 20.3 - 4;
     }
     const compareLoc = curPriceDis - 33;
     percentage = Math.floor((currentPrice / averagePrice - 1) * 100);
@@ -73,35 +79,35 @@ class AveragePrice extends React.Component {
       compare = `${Math.abs(percentage)}% Lower`;
     }
     return (
-  <div className="Components">
-   <p className='topic'>Price Paid on Robinhood</p>
-   <div className='line'></div>
-   <div id = 'compare' style={{ left: compareLoc }}>
-    <div style={{ position: 'absolute'}}>
-      <p id = 'compare'>{compare}</p>
-      <p id = 'rightNow'>Right Now</p>
-    </div>
-   </div>
-   <div id = 'chart' >
-    {allData.map(priceData => <Chart key = {priceData[3]} priceData = {priceData} />)}
-   </div>
-   <div className='bottomLine'>
-    <span id = 'bottomFrontLine' style={{ width: curPriceDis > 670 ? 670 : curPriceDis }}></span>
-    <span id = 'circle' ></span>
-    <span id = 'bottomRareLine' style={{ width: 670 - curPriceDis }}></span>
-   </div>
+      <div className="Components">
+        <p className='topic'>Price Paid on Robinhood</p>
+        <div className='line'></div>
+        <div id='compare' style={{ left: compareLoc }}>
+          <div style={{ position: 'absolute' }}>
+            <p id='compare'>{compare}</p>
+            <p id='rightNow'>Right Now</p>
+          </div>
+        </div>
+        <div id='chart' >
+          {allData.map(priceData => <Chart key={priceData[3]} priceData={priceData} />)}
+        </div>
+        <div className='bottomLine'>
+          <span id='bottomFrontLine' style={{ width: curPriceDis > 670 ? 670 : curPriceDis }}></span>
+          <span id='circle' ></span>
+          <span id='bottomRareLine' style={{ width: 670 - curPriceDis }}></span>
+        </div>
 
-    <div style={{ display: 'inline-block', width: '670px' }}>
-      <div id = 'lowest'>52 Week Low
+        <div style={{ display: 'inline-block', width: '670px' }}>
+          <div id='lowest'>52 Week Low
       <p id='lowest'>${lowest}</p></div>
-      <div id = 'averagePricePaid' style={{ marginLeft: avgPriceDis ? avgPriceDis : 0 }}>
-        <p className='averagePricePaid'>Average Price Paid</p>
-        <p className='averagePricePaid' id='averagePricePaid'>${averagePrice}</p>
-      </div>
-      <div id = 'highest'>52 Week High
+          <div id='averagePricePaid' style={{ marginLeft: avgPriceDis ? avgPriceDis : 0 }}>
+            <p className='averagePricePaid'>Average Price Paid</p>
+            <p className='averagePricePaid' id='averagePricePaid'>${averagePrice}</p>
+          </div>
+          <div id='highest'>52 Week High
       <p id='highest'>${highest}</p></div>
-    </div>
-  </div>
+        </div>
+      </div>
     );
   }
 }
